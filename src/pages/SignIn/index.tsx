49287ad75a447bb5e9dc52ca47c4463501e750f5
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-08-16 18:31:36 -0300

    criado novos componentes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -13,10 +16,18 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu logon</h1>
 
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <Input 
+          name="email" 
+          type="text" 
+          placeholder="E-mail" 
+          icon={FiMail} />
+        <Input 
+          name="password" 
+          type="password" 
+          placeholder="Senha" 
+          icon={FiLock} />
 
-        <button>Entrar</button>
+        <Button>Entrar</Button>
 
         <a href="/forgot">Esqueci minha senha</a>
       </form>
