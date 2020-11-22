bd3821667de8d765c852b2639df74d2a3d2a2a22
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-11-22 17:37:48 -0300

    create new components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -12,10 +13,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface ISignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const { signIn } = useAuth();
+
+  const handleSubmit = useCallback(async (data: ISignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -30,11 +38,15 @@ const SignIn: React.FC = () => {
         abortEarly: false
       });
 
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
     } catch (err) {
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }
-  }, [])
+  }, [signIn])
 
   return (
     <Container>
